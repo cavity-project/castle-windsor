4c6e3783540eefd6f5b6014684a11dcd4a3dc3ab
adean <alan.dean@gmail.com>
2016-04-19 20:48:50 +0100

    Ported from Google Code Project.

---
+++ src/Class Libraries/ServiceLocation.CastleWindsor.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyDefaultAlias("Cavity.ServiceLocation.CastleWindsor.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.ServiceLocation.CastleWindsor.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Castle Windsor Service Location Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Castle Windsor Service Location Facts Library (Release)")]
+
+#endif
---
+++ src/Class Libraries/ServiceLocation.CastleWindsor/Configuration/XmlServiceLocatorProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace Cavity.Configuration
+{
+    using System.Diagnostics;
+    using Castle.Windsor;
+    using Castle.Windsor.Configuration.Interpreters;
+    using Cavity.Diagnostics;
+    using Cavity.Practices.ServiceLocation;
+    using Microsoft.Practices.ServiceLocation;
+
+    public sealed class XmlServiceLocatorProvider : ISetLocatorProvider
+    {
+        public void Configure()
+        {
+            Trace.WriteIf(Tracing.Is.TraceVerbose, string.Empty);
+            var container = new WindsorContainer(new XmlInterpreter());
+            ServiceLocator.SetLocatorProvider(() => new WindsorServiceLocator(container));
+        }
+    }
+}
